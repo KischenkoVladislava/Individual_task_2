541cec48f470f1d05472f17c666ac3a5eecb56de
Igor <Igor@DESKTOP-LL1G0RV>
2021-10-18 00:18:28 +0300

    Добавьте файлы проекта.

---
+++ Individual_task_2/Individual_task_2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    double a, b, x, w, y;
+    cout << "Для решения w = sqrt(x^2+b)-(b^2)*((sin(x+a))^3)/ x и " << endl << "y = ((cos(x ^ 3)) ^ 2) - x / sqrt(a ^ 2 + b ^ 2) введите значения парамеьров a, b и x" << endl;
+    cin >> a >> b >> x;
+    while (x == 0)
+    {
+        cout << "При делении на ноль получается неопределенное выражение. Введите значения парамеьров a, b и x снова" << endl;
+        cin >> a >> b >> x;
+    }
+    w = sqrt(x * x + b) - (b * b) * ((sin(x + a)) * (sin(x + a)) * (sin(x + a))) / x;
+    y = ((cos(x * x * x)) * (cos(x * x * x))) - x / sqrt(a * a + b * b);
+    cout << "Ответ:" << w << ", " << y;
+    return 0;
+}
